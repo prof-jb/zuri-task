494e5594aa6d2351417db9e7d6af7a951a8ebd77
profjb <tayojubril1@gmail.com>
2022-06-15 23:24:50 +0100

    second commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+user = get_user_model()
+
+# Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    
+    author = models.ForeignKey(user,on_delete=models.CASCADE)
+    created_date =  models.DateTimeField()
+    published_date =  models.DateTimeField()
+  
